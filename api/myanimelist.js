c8c185c1ad88bdb00b60c64798c6e98d21caa0c9
James Harris <wopiancitizen123@gmail.com>
2017-03-14 22:53:10 +0000

    Update

--- api/myanimelist.js
+++ api/myanimelist.js
@@ -2,6 +2,7 @@ import env from '../env'
 import sleep from 'thread-sleep'
 import chalk from 'chalk'
 import pad from 'pad-left'
+import Q from 'q'
 import MALjs from 'MALjs'
 
 const log = console.log
@@ -9,33 +10,56 @@ const log = console.log
 export default function myanimelist () {
     log(chalk.blue('MyAnimeList Library Filler'))
     log(chalk.blue(`user\t${env.username}\n`))
-    var api = new MALjs(env.username, env.password);
+
+    const api = new MALjs(env.username, env.password);
+    let i = 1;
 
     function add (i) {
+        const deferred = Q.defer()
         const padded = pad(i, 5, '0')
 
         log(`${padded}\t${chalk.yellow('request')}`)
+
         api.anime.add(i, {
             status: '2' // completed
         })
-        .then(result => log(`${padded}\t${chalk.green('added')}`))
+        .then(result => {
+            log(`${padded}\t${chalk.green('added')}`)
+            deferred.resolve();
+        })
         .catch(err => update(i))
+
+        return deferred.promise
     }
 
     function update(i) {
+        const deferred = Q.defer()
         const padded = pad(i, 5, '0')
 
         log(`${padded}\t${chalk.magenta('update')}`)
+
         api.anime.update(i, {
             status: '2' // completed
         })
-        .then(result => log(`${padded}\t${chalk.green('updated')}`))
+        .then(result => {
+            log(`${padded}\t${chalk.green('updated')}`)
+            deferred.resolve();
+        })
         .catch(err => log(`${padded}\t${chalk.red('failed')}`))
+
+        return deferred.promise
     }
 
-    // for (let i = 1; i < 35087; i++) {
-    for (let i = 1; i < 5; i++) {
-        add(i)
-        sleep(1000)
+    function run () {
+        var deferred = Q.defer();
+        deferred.resolve();
+        return deferred.promise;
+    }
+
+    while (i < 35087) {
+        run()
+        .then(add(i))
+        .then(i++)
+        .then(sleep(1000))
     }
 }
