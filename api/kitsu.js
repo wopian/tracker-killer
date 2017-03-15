59f9f97aff8c90fc4dfb4d303c641d28c3de42eb
matthewdias <matthewdias@me.com>
2017-03-15 03:47:28 -0500

    kitsu auth

--- api/kitsu.js
+++ api/kitsu.js
@@ -2,15 +2,24 @@ import { KITSU } from '../env'
 import { log } from '../util'
 import chalk from 'chalk'
 import axios from 'axios'
+import OAuth2 from 'client-oauth2'
 
-export default function kitsu () {
+const baseUrl = 'https://kitsu.io/api'
+
+export default async function kitsu () {
   log(chalk.blue('Kitsu Library Filler'))
   log(chalk.blue(`user\t${KITSU.username}\n`))
 
-  const auth = axios({
-    headers: {
-      'Accept': 'application/vnd.api+json',
-      'Content-Type': 'application/vnd.api+json'
-    }
+  const oauth = new OAuth2({
+    clientId: KITSU.client_id,
+    clientSecret: KITSU.client_secret,
+    accessTokenUri: baseUrl + '/oauth/token'
   })
+
+  const { accessToken } = await oauth.owner.getToken(KITSU.username, KITSU.password)
+  const headers = {
+    'Accept': 'application/vnd.api+json',
+    'Content-Type': 'application/vnd.api+json',
+    'Authorization': 'Bearer ' + accessToken
+  }
 }
