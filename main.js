967dc10f15b1f7f818de0482d4e43f4ae8965994
James Harris <wopiancitizen123@gmail.com>
2017-03-15 07:26:45 +0000

    refactor: use log from util

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import input from 'inquirer'
+import { log } from './util'
 import anidb from './api/anidb'
 import animeplanet from './api/animeplanet'
 import anilist from './api/anilist'
 import annict from './api/annict'
 import kitsu from './api/kitsu'
 import myanimelist from './api/myanimelist'
-
-const log = console.log
 
 input.prompt([
     {
