99f25c402847c2a3adc2bcf6d7e9de01f9e4d709
James Harris <wopiancitizen123@gmail.com>
2017-03-15 02:10:09 +0000

    feat: ask for service to test

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+import input from 'inquirer'
 import myanimelist from './api/myanimelist'
 
-myanimelist()
+const log = console.log
+
+input.prompt([
+    {
+        type: 'list',
+        name: 'service',
+        message: 'Select service to test:',
+        choices: [
+            'AniList',
+            'Kitsu',
+            'MyAnimeList'
+        ]
+    }
+])
+.then(answers => {
+    log(JSON.stringify(answers, null, '  '))
+})
+
+// myanimelist()
