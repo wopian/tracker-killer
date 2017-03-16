8cfac9308dce2c8dba08163475aa9f30230cdd62
James Harris <wopiancitizen123@gmail.com>
2017-03-16 11:06:00 +0000

    refactor: camelCase env tokens

--- api/kitsu.js
+++ api/kitsu.js
@@ -1,20 +1,28 @@
-import axios from 'axios'
+import JsonAPi from 'devour-client'
 import OAuth2 from 'client-oauth2'
 import { KITSU } from '../env'
-import { log, top } from '../util'
+import { top } from '../util'
+import { version } from '../package'
 
 const baseUrl = 'https://kitsu.io/api'
 
 export default async function kitsu (service, type) {
   top(service, type, KITSU.username)
 
-  const oauth = new OAuth2({
-    clientId: KITSU.client_id,
-    clientSecret: KITSU.client_secret,
+  const auth = new OAuth2({
+    clientId: KITSU.clientId,
+    clientSecret: KITSU.clientSecret,
     accessTokenUri: baseUrl + '/oauth/token'
   })
 
-  const { accessToken } = await oauth.owner.getToken(KITSU.username, KITSU.password)
+  const Kitsu = new JsonAPi({
+    apiUrl: `${baseUrl}/edge`,
+    logger: false
+  })
+
+  Kitsu.headers['User-Agent'] = `tracker-killer/${version}`
+
+  const { accessToken } = await auth.owner.getToken(KITSU.username, KITSU.password)
   const headers = {
     'Accept': 'application/vnd.api+json',
     'Content-Type': 'application/vnd.api+json',
